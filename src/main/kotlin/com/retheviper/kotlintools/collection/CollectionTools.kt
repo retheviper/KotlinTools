d3cbf8c4d44196837568eefc2bdfea46189c3e28
retheviper <retheviper@gmail.com>
2021-09-06 13:29:57 +0900

    little fix

--- src/main/kotlin/com/retheviper/kotlintools/collection/CollectionTools.kt
+++ src/main/kotlin/com/retheviper/kotlintools/collection/CollectionTools.kt
@@ -1,12 +1,6 @@
 package com.retheviper.kotlintools.collection
 
 /**
- * Concat List witch was split by [List.chunked].
- */
-fun <T> List<List<T>>.flatten(): List<T> =
-    reduce { acc, list -> acc + list }
-
-/**
  * Swap index of elements of given indices.
  */
 fun <T> List<T>.swappedByIndex(indexFrom: Int, indexTo: Int): List<T> =
